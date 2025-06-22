3ae7d3c0dcacdde8f570fac8a1642e48eb43691f
ErEr <jeffliao92316046@gmail.com>
2025-06-22 23:13:50 +0800

    feat: add CRAWLER_MODE to environment configuration and update settings

--- src/core/config.py
+++ src/core/config.py
@@ -3,15 +3,18 @@ from pydantic_settings import BaseSettings
 
 load_dotenv()
 
+
 class Settings(BaseSettings):
     POSTGRES_SERVER: str
     POSTGRES_PORT: int
     POSTGRES_USERNAME: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
+    CRAWLER_MODE: str
 
     @property
     def database_url(self):
         return f"postgresql://{self.POSTGRES_USERNAME}:{self.POSTGRES_PASSWORD}@{self.POSTGRES_SERVER}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+
 
 settings = Settings()
