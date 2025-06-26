8679a4669a7c9c237287d60a2a55f101f7931fa8
ErEr <jeffliao92316046@gmail.com>
2025-06-26 23:03:33 +0800

    feat: add course and teacher routers to API

--- src/api/router.py
+++ src/api/router.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
 
-from api.routers import auth,swagger_oauth
+from api.routers import auth, swagger_oauth, course, teacher
 
 from core.config import settings
+
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth")
-
+api_router.include_router(course.router, prefix="/course")
+api_router.include_router(teacher.router, prefix="/teacher")
 if settings.APP_MODE == "dev":
     api_router.include_router(swagger_oauth.oauth_router, prefix="/auth")
