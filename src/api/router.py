71452e199519b87c8dfed98c56e024faef644691
ErEr <jeffliao92316046@gmail.com>
2025-06-27 23:59:33 +0800

    fix: update API route prefixes for consistency in router.py

--- src/api/router.py
+++ src/api/router.py
@@ -13,10 +13,10 @@ from core.config import settings
 
 api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth")
-api_router.include_router(course.router, prefix="/course")
-api_router.include_router(teacher.router, prefix="/teacher")
-api_router.include_router(course_selection.router, prefix="/course-selection")
-api_router.include_router(course_table.router, prefix="/course-table")
+api_router.include_router(course.router, prefix="/courses")
+api_router.include_router(teacher.router, prefix="/teachers")
+api_router.include_router(course_selection.router, prefix="/course-selections")
+api_router.include_router(course_table.router, prefix="/course-tables")
 
 if settings.APP_MODE == "dev":
     api_router.include_router(swagger_oauth.oauth_router, prefix="/auth")
