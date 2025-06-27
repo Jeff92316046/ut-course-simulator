472901d49237c7e9a033d44cf36ffa7e8e438fc1
ErEr <jeffliao92316046@gmail.com>
2025-06-27 21:15:14 +0800

    feat: add course_selection and course_table routers to API

--- src/api/router.py
+++ src/api/router.py
@@ -1,6 +1,13 @@
 from fastapi import APIRouter
 
-from api.routers import auth, swagger_oauth, course, teacher
+from api.routers import (
+    auth,
+    swagger_oauth,
+    course,
+    teacher,
+    course_selection,
+    course_table,
+)
 
 from core.config import settings
 
@@ -8,5 +15,8 @@ api_router = APIRouter()
 api_router.include_router(auth.router, prefix="/auth")
 api_router.include_router(course.router, prefix="/course")
 api_router.include_router(teacher.router, prefix="/teacher")
+api_router.include_router(course_selection.router, prefix="/course-selection")
+api_router.include_router(course_table.router, prefix="/course-table")
+
 if settings.APP_MODE == "dev":
     api_router.include_router(swagger_oauth.oauth_router, prefix="/auth")
